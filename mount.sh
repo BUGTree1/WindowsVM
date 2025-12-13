b53270c6f70c7af2f7576ce0751ef602956af308
BUGTree1 <szymek2017r@gmail.com>
2025-12-13 20:48:52 +0100

    mount

--- mount.sh
+++ mount.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-LINUX_ISO="void-live-base.iso"
+LINUX_ISO="linux.iso"
 
 qemu-system-x86_64 -enable-kvm -machine q35 -smp 4 -cdrom "$LINUX_ISO" -m 4G -drive "file=fat:rw:virt" -hdb win11.qcow2 -boot d -cpu host
